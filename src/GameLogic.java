1534d6c0e38fe18b06fea51b5eadbdd5ff1f64ae
YUXIN HU <156398873+yx9299@users.noreply.github.com>
2024-04-15 12:42:29 -0700

    Update GameLogic.java

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,3 +1,31 @@
 public class GameLogic {
+    private GameBoard board;
+    private GameBoard newboard;
+
+    public GameLogic(GameBoard board){
+        // initial game: all cells are die
+        this.board = board;
+    }
+
+    public void addCell(int x, int y){
+        // user click cell and change the cell status
+        this.board.cellChangeState(x,y);
+    }
+
+    public void getNewBoard(){
+        Generation nextGen = new Generation(board);
+        nextGen.newGeneration();
+        newboard  = nextGen.getNewBoard();
+    }
+
+    public GameBoard getNewboard(){
+        return newboard;
+    }
+
+    public GameBoard getBoard(){
+        return board;
+    }
+
+
 
 }
