28bf58a616cda369ee7738f35519737ce979841c
YUXIN HU <156398873+yx9299@users.noreply.github.com>
2024-04-15 12:41:54 -0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+       GameBoard board = new GameBoard(8);
+        GameLogic game = new GameLogic(board);
+
+        //Cell c1 = new Cell(board, 1,1);
+
+        game.addCell(1,1);
+        game.addCell(1,2);
+        game.addCell(1,3);
+        System.out.print(game.getBoard().toString());
+        System.out.print("------\n");
+        game.getNewBoard();
+        System.out.print(game.getNewboard().toString())
     }
 }
