75c1b7cb30656b95846525a5cec576c3ac14d150
YUXIN HU <156398873+yx9299@users.noreply.github.com>
2024-04-15 12:43:15 -0700

    Update GameBoard.java

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,8 +1,7 @@
 public class GameBoard {
     // 2 dimensional array
     private Cell[][] grid;
-
-    // the side length of the game board
+    // the length size of the game board
     private int size;
 
     public GameBoard(int size) {
@@ -15,20 +14,42 @@ public class GameBoard {
         }
     }
 
+    public GameBoard(Cell[][] newGird){
+        this.grid = newGird;
+        this.size = newGird.length;
+    }
+
+    public void copyCell(Cell cell){
+        grid[cell.getX()][cell.getY()] = cell;
+    }
+
+
+
+    public void cellChangeState(int x, int y){
+        grid[x][y].switchState();
+    }
+
     public int getSize() {
         return size;
     }
     public Cell[][] getGrid() {
         return grid;
     }
-    public void update() {
-        // call the function in GameLogic to update the gameBoard
-    }
-    public void reset() {
-        for (int i = 0; i < this.size; i++) {
-            for (int j = 0; j < this.size; j++) {
-                this.getGrid()[i][j].setStatus(false);
+
+    public String toString(){
+        StringBuilder stringGird = new StringBuilder();
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++){
+                if(grid[i][j].isAlive()){
+                    stringGird.append("1");
+                }else{
+                    stringGird.append("0");
+                }
+                stringGird.append("\t");
             }
+            stringGird.append("\n");
         }
+        return stringGird.toString();
     }
+
 }
