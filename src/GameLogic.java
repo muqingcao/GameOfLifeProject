4971fd82a2fa51207233fa59f465f50d0ddad8bf
YUXIN HU <156398873+yx9299@users.noreply.github.com>
2024-04-17 11:16:47 -0700

    Update the continue get new version

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,6 +1,13 @@
+import java.util.Arrays;
+
 public class GameLogic {
     private GameBoard board;
     private GameBoard newboard;
+
+    private GameBoard boardPrev;
+    private GameBoard boardCurr;
+
+
 
     public GameLogic(GameBoard board){
         // initial game: all cells are die
@@ -12,10 +19,26 @@ public class GameLogic {
         this.board.cellChangeState(x,y);
     }
 
-    public void getNewBoard(){
+    public void getNextBoard(){
         Generation nextGen = new Generation(board);
         nextGen.newGeneration();
         newboard  = nextGen.getNewBoard();
+    }
+
+    public void newBoardContinue(){
+        String prev = "null";
+        String curr = this.board.toString();
+        boardPrev = board;
+
+        while(!prev.equals(curr)){
+            Generation nextGen = new Generation(boardPrev);
+            nextGen.newGeneration();
+            boardCurr  = nextGen.getNewBoard();
+            prev = curr;
+            curr = this.boardCurr.toString();
+            boardPrev = boardCurr;
+        }
+
     }
 
     public GameBoard getNewboard(){
@@ -26,6 +49,12 @@ public class GameLogic {
         return board;
     }
 
+    public GameBoard getBoardCurr(){
+        return boardCurr;
+    }
+    public GameBoard getBoardPrev(){
+        return boardPrev;
+    }
 
 
 }
